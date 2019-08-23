2ac2f4ca75b2db742bd6d9200b96286f3dc692f7
Carlos Eduardo Pereira Marques <ra214327@brasil.lab.ic.unicamp.br>
2019-08-23 11:14:44 -0300

    Metrônomo criado

---
+++ scripts/Game.gd
@@ -0,0 +1,59 @@
+extends Node2D
+
+onready var background_panel = $Background
+onready var signal_panel = $Signal
+
+var rythm_timer = Timer.new()
+var rythm_threshold_timer = Timer.new()
+
+var press_time = false
+
+func _ready():
+	# Definir e adicionar estilos (cores) para os painéis
+	var background_style = StyleBoxFlat.new()
+	var signal_style = StyleBoxFlat.new()
+	
+	background_style.set_bg_color(Color(0,0,0))
+	signal_style.set_bg_color(Color(1,1,1))
+	
+	background_panel.add_stylebox_override("panel", background_style)
+	signal_panel.add_stylebox_override("panel", signal_style)
+	
+	background_panel.update()
+	signal_panel.update()
+	
+	# Definir o ritmo do jogo
+	rythm_timer.set_wait_time(1)
+	rythm_timer.connect("timeout", self, "_on_rythm_timeout")
+	add_child(rythm_timer)
+	rythm_timer.start()
+	
+	# Definir o limite de reação do usuário
+	rythm_threshold_timer.set_wait_time(0.5)
+	rythm_threshold_timer.connect("timeout", self, "_on_threshold_timeout")
+	add_child(rythm_threshold_timer)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		if press_time:
+			print("HIT!")
+		else:
+			print("NO!")
+
+func _on_rythm_timeout():
+	# Exibir o quadrado branco
+	signal_panel.show()
+	
+	# O jogador deve reagir
+	press_time = true
+	
+	# Começar a contagem do ritmo e do limite de reação
+	rythm_timer.start()
+	rythm_threshold_timer.start()
+
+func _on_threshold_timeout():
+	# Esconder o quadrado branco
+	signal_panel.hide()
+	
+	# O jogador não deve mais pressionar
+	press_time = false
