e899827d1b6e7bb7a830305f8640361e5910bbf6
Carlos Eduardo Pereira Marques <ra214327@belize.lab.ic.unicamp.br>
2019-08-26 18:50:14 -0300

    Definindo o contratempo (ainda tá errado)

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 onready var background_panel = $Background
-onready var signal_panel = $Signal
+onready var rythm_signal_panel = $RythmSignal
+onready var setback_signal_panel = $SetbackSignal
 
 var rythm_timer = Timer.new()
 var rythm_threshold_timer = Timer.new()
+var rythm_setback_delay_timer = Timer.new()
+var rythm_setback_timer = Timer.new()
 
 var press_time = false
 
@@ -13,16 +16,20 @@ onready var character = $Character
 func _ready():
 	# Definir e adicionar estilos (cores) para os painéis
 	var background_style = StyleBoxFlat.new()
-	var signal_style = StyleBoxFlat.new()
+	var rythm_signal_style = StyleBoxFlat.new()
+	var setback_signal_style = StyleBoxFlat.new()
 	
 	background_style.set_bg_color(Color(0,0,0))
-	signal_style.set_bg_color(Color(1,1,1))
+	rythm_signal_style.set_bg_color(Color(1,1,1))
+	setback_signal_style.set_bg_color(Color(0,0,.75))
 	
 	background_panel.add_stylebox_override("panel", background_style)
-	signal_panel.add_stylebox_override("panel", signal_style)
+	rythm_signal_panel.add_stylebox_override("panel", rythm_signal_style)
+	setback_signal_panel.add_stylebox_override("panel", setback_signal_style)
 	
 	background_panel.update()
-	signal_panel.update()
+	rythm_signal_panel.update()
+	setback_signal_panel.update()
 	
 	# Definir o ritmo do jogo
 	rythm_timer.set_wait_time(0.8)
@@ -34,6 +41,16 @@ func _ready():
 	rythm_threshold_timer.set_wait_time(0.4)
 	rythm_threshold_timer.connect("timeout", self, "_on_threshold_timeout")
 	add_child(rythm_threshold_timer)
+	
+	# Definir o delay do contratempo
+	rythm_setback_delay_timer.set_wait_time(0.1)
+	rythm_setback_delay_timer.connect("timeout", self, "_on_setback_delay_timeout")
+	add_child(rythm_setback_delay_timer)
+	
+	# Definir o contratempo
+	rythm_setback_timer.set_wait_time(0.2)
+	rythm_setback_timer.connect("timeout", self, "_on_setback_timeout")
+	add_child(rythm_setback_timer)
 
 func _input(event):
 	# Fazer o personagem pular caso o jogador acerte
@@ -46,7 +63,7 @@ func _input(event):
 
 func _on_rythm_timeout():
 	# Exibir o quadrado branco
-	signal_panel.show()
+	rythm_signal_panel.show()
 	
 	# O jogador deve reagir
 	press_time = true
@@ -56,8 +73,26 @@ func _on_rythm_timeout():
 	rythm_threshold_timer.start()
 
 func _on_threshold_timeout():
+	print('chamou')
 	# Esconder o quadrado branco
-	signal_panel.hide()
+	rythm_signal_panel.hide()
+	
+	# O jogador não deve mais pressionar
+	press_time = false
+	
+	rythm_setback_delay_timer.start()
+
+func _on_setback_delay_timeout():
+	# Mostrar o quadrado azul
+	setback_signal_panel.show()
+	
+	# Iniciar o tempo de resposta do contratempo
+	rythm_setback_timer.start()
+	press_time = true
+
+func _on_setback_timeout():
+	# Esconder o quadrado azul
+	setback_signal_panel.hide()
 	
 	# O jogador não deve mais pressionar
 	press_time = false
