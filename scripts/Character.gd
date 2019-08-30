2e31fc327a2e9cc96f5df8a45aba4b14a9f4d97c
Carlos Eduardo Pereira Marques <ra214327@argentina.lab.ic.unicamp.br>
2019-08-30 15:59:12 -0300

    Metrônomo 2.0 finalizado e ajustes no projeto

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -1,15 +1,30 @@
 extends KinematicBody2D
 
-const GRAVITY = 100000
+const GRAVITY = 500000
 
 var velocity = Vector2()
-export var jump_velocity = 30000
+export var jump_velocity = 80000
+
+onready var game = get_parent()
 
 func _physics_process(delta):
 	# Gravidade do jogo
 	velocity.y += delta * GRAVITY
 	var motion = velocity * delta
 	motion = move_and_slide(motion, Vector2(0,-1))
+
+func _input(event):
+	# Fazer o personagem pular caso o jogador acerte
+	if event.is_action_pressed("ui_accept"):
+		if game.press_time:
+			if not game.p1_pressed:
+				print("HIT!")
+				game.p1_pressed = true
+				jump()
+			else:
+				print("NO!")
+		else:
+			print("NO!")
 
 # Se o jogador estiver no chão, aplicar uma força vertical
 func jump():
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -4,14 +4,12 @@ onready var background_panel = $Background
 onready var rythm_signal_panel = $RythmSignal
 onready var setback_signal_panel = $SetbackSignal
 
-var rythm_timer = Timer.new()
-var rythm_threshold_timer = Timer.new()
-var rythm_setback_delay_timer = Timer.new()
-var rythm_setback_timer = Timer.new()
+export var bpm = 80
+var game_time = 0
 
 var press_time = false
-
-onready var character = $Character
+var p1_pressed = false
+var p2_pressed = false
 
 func _ready():
 	# Definir e adicionar estilos (cores) para os painéis
@@ -30,69 +28,24 @@ func _ready():
 	background_panel.update()
 	rythm_signal_panel.update()
 	setback_signal_panel.update()
-	
-	# Definir o ritmo do jogo
-	rythm_timer.set_wait_time(0.8)
-	rythm_timer.connect("timeout", self, "_on_rythm_timeout")
-	add_child(rythm_timer)
-	rythm_timer.start()
-	
-	# Definir o limite de reação do usuário
-	rythm_threshold_timer.set_wait_time(0.4)
-	rythm_threshold_timer.connect("timeout", self, "_on_threshold_timeout")
-	add_child(rythm_threshold_timer)
-	
-	# Definir o delay do contratempo
-	rythm_setback_delay_timer.set_wait_time(0.1)
-	rythm_setback_delay_timer.connect("timeout", self, "_on_setback_delay_timeout")
-	add_child(rythm_setback_delay_timer)
-	
-	# Definir o contratempo
-	rythm_setback_timer.set_wait_time(0.2)
-	rythm_setback_timer.connect("timeout", self, "_on_setback_timeout")
-	add_child(rythm_setback_timer)
 
-func _input(event):
-	# Fazer o personagem pular caso o jogador acerte
-	if event.is_action_pressed("ui_accept"):
-		if press_time:
-			print("HIT!")
-			character.jump()
-		else:
-			print("NO!")
+func _process(delta):
+	game_time += delta
+	rythm_update()
 
-func _on_rythm_timeout():
-	# Exibir o quadrado branco
-	rythm_signal_panel.show()
+func rythm_update():
+	var rythm_time = fmod(game_time, 60.0/bpm)/(60.0/bpm)
 	
-	# O jogador deve reagir
-	press_time = true
-	
-	# Começar a contagem do ritmo e do limite de reação
-	rythm_timer.start()
-	rythm_threshold_timer.start()
-
-func _on_threshold_timeout():
-	print('chamou')
-	# Esconder o quadrado branco
-	rythm_signal_panel.hide()
-	
-	# O jogador não deve mais pressionar
-	press_time = false
-	
-	rythm_setback_delay_timer.start()
-
-func _on_setback_delay_timeout():
-	# Mostrar o quadrado azul
-	setback_signal_panel.show()
-	
-	# Iniciar o tempo de resposta do contratempo
-	rythm_setback_timer.start()
-	press_time = true
-
-func _on_setback_timeout():
-	# Esconder o quadrado azul
-	setback_signal_panel.hide()
-	
-	# O jogador não deve mais pressionar
-	press_time = false
+	# Ligar press_time no tempo e no contratempo
+	if rythm_time <= 0.5:
+		press_time = true
+		rythm_signal_panel.show()
+	elif rythm_time > 0.625 and rythm_time < 0.875:
+		press_time = true
+		setback_signal_panel.show()
+	else:
+		press_time = false
+		p1_pressed = false
+		p2_pressed = false
+		rythm_signal_panel.hide()
+		setback_signal_panel.hide()
